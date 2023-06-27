9f592a73757089d31f4a65b20a09c0bdabb39886
Chris Garvin <chrisjoelgarvin@gmail.com>
2023-06-27 15:15:56 +1000

    added dmenu color schemes

--- dmenu-flexipatch/colors/gruvbox.h
+++ dmenu-flexipatch/colors/gruvbox.h
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-/* Everforest colors */
  char *colors[][2] = {
          /*               fg         bg       */
          [SchemeNorm] = { "#fbf1c7", "#322f2e" },
@@ -8,12 +6,3 @@
          #if BORDER_PATCH
          [SchemeBorder] = { "#665c54", "#665c54" },
          #endif // BORDER_PATCH
-=======
-        /*               fg         bg       */
-        [SchemeNorm] = { "#fbf1c7", "#2b2b2b" },
-        [SchemeSel]  = { "#2b2b2b", "#665c54" },
-        [SchemeOut]  = { "#fb4934", "#312e2d" },
-        #if BORDER_PATCH
-        [SchemeBorder] = { "#665c54", "#665c54" },
-        #endif // BORDER_PATCH
->>>>>>> c27ab01 (added dmenu color schemes)
