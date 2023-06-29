3c7d82f6acf78c1eaa8b496c1ea9061d1b190d98
Chris Garvin <chrisjoelgarvin@gmail.com>
2023-06-30 07:48:50 +1000

    add default color scheme for st and dmenu

--- dmenu-flexipatch/colors/default.h
+++ dmenu-flexipatch/colors/default.h
@@ -2,7 +2,7 @@
  char *colors[][2] = {
          /*               fg         bg       */
          [SchemeNorm] = { "#eceff4", "#2e3440" },
-         [SchemeSel]  = { "#eceff4", "#4c566a" },
+         [SchemeSel]  = { "#eceff4", "#39404f" },
          [SchemeOut]  = { "#fb4934", "#312e2d" },
          #if BORDER_PATCH
          [SchemeBorder] = { "#7a8478", "#7a8478" },
